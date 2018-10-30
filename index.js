7fe6e10d7e3df978f7a32e1515699cfd11472d97
Natalie Halawani <hala1224@gmail.com>
2018-10-30 22:48:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 
+class Driver {
+  
+   constructor(name, startDate) {
+     this.name = name;
+     this.startDate = new Date(startDate);
+   }
+   
+
+
+   yearsExperienceFromBeginningOf(year) {
+    
+     let answer=parseInt(year)-this.startDate.getFullYear();
+      return answer;  
+   }
+}
+   class Route {
+     
+     constructor(beginningLocation, endLocation){
+       this.beginningLocation = beginningLocation;
+       this.endLocation = endLocation;
+       
+     }
+     
+     blocksTravelled(){
+       let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+       this.beginningLocation.horizontal
+     }
+       
+    
+   }
